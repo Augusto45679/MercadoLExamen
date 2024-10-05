de14c421213ad6c03379b7ae64f2f17fd794387c
Augusto Berloin <nfunes706@gmail.com>
2024-10-04 21:07:38 -0300

    21.07

--- src/main/java/org/example/controllers/MutantStatsController.java
+++ src/main/java/org/example/controllers/MutantStatsController.java
@@ -1,10 +1,16 @@
 package org.example.controllers;
 
 import org.example.entities.MutantStats;
+import org.example.services.MutantServiceImpl;
+import org.example.services.MutantStatsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = " ")
-public class MutantStatsController extends BaseControllerImpl<MutantStats,Long> {
+public class MutantStatsController extends BaseControllerImpl<MutantStats, MutantStatsServiceImpl> {
+    
+    @Autowired
+    private MutantStats mutantStats;
 }
--- src/main/java/org/example/services/MutantStatsServiceImpl.java
+++ src/main/java/org/example/services/MutantStatsServiceImpl.java
@@ -3,6 +3,7 @@ package org.example.services;
 
 import org.example.entities.MutantStats;
 import org.example.repositories.BaseRepository;
+import org.example.repositories.MutantStatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,8 @@ import java.util.List;
 @Service
 public class MutantStatsServiceImpl extends BaseServiceImpl<MutantStats,Long>{
 
+    @Autowired
+    private MutantStatsRepository mutantStatsRepository;
 
     public MutantStatsServiceImpl(BaseRepository<MutantStats, Long> baserepository) {
         super(baserepository);
